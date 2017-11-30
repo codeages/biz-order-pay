a3f6d299716556477900c715d57fa0aa8544effd
adili <adili@howzhi.com>
2017-11-30 14:43:55 +0800

    为运输类型增加数据库脚本

--- src/Order/Command/TableCommand.php
+++ src/Order/Command/TableCommand.php
@@ -22,7 +22,7 @@ class TableCommand extends AbstractCommand
         $directory = $input->getArgument('directory');
 
         $migrations = array(
-            'order',
+            'order', 'shipping_type'
         );
 
         foreach ($migrations as $migration) {
