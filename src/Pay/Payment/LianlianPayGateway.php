cff21e16a70ab605a3288217bd26878526bc6bd4
shenchupeng <shenchupeng@howzhi.com>
2018-04-09 10:39:22 +0800

    连连支付发送信息加上手机号

--- src/Pay/Payment/LianlianPayGateway.php
+++ src/Pay/Payment/LianlianPayGateway.php
@@ -133,10 +133,14 @@ class LianlianPayGateway extends AbstractGateway
         if (!empty($params['return_url'])) {
             $converted['url_return'] = $params['return_url'];
         }
+        $userId = array_pop(explode("_", $converted['user_id']));
+        $user = $this->getUserService()->getUser($userId);
+        $bindPhone = $this->processBindPhone($user);
         $converted['risk_item']  = json_encode(array(
             'frms_ware_category'=>1008,
             'user_info_mercht_userno'=>$params['attach']['identify_user_id'],
-            'user_info_dt_register'=>date('YmdHis', $params['attach']['user_created_time'])
+            'user_info_dt_register'=>date('YmdHis', $params['attach']['user_created_time']),
+            'user_info_bind_phone' => $bindPhone
         ));
 
         $converted['userreq_ip'] = str_replace(".", "_", $params['create_ip']);
@@ -185,5 +189,21 @@ class LianlianPayGateway extends AbstractGateway
             'oid_partner' => $config['oid_partner'],
             'signatureToolkit' => $config['signatureToolkit'],
         );
+    }
+
+    protected function getUserService()
+    {
+        return $this->biz->service('User:UserService');
+    }
+
+    private function processBindPhone($user)
+    {
+        $bindPhone = '';
+        if (!empty($user['verifiedMobile'])) {
+            $head = substr($user['verifiedMobile'], 0, 3);
+            $tail = substr($user['verifiedMobile'], -4, 4);
+            $bindPhone = $head.'****'.$tail;
+        }
+        return $bindPhone;
     }
 }
